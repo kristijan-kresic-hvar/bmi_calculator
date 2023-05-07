de52c740a8e3034df42a75cabb255d7d433ee1ce
kkresic <kristijan@agilno.com>
2023-05-07 06:35:48 +0200

    Create header ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
+import Image from 'next/image'
+import logoSVG from '@/assets/img/logo.svg'
+
 export default function Home() {
     return (
-        <div className="">
-            <h1 className="text-gunmetal">Home</h1>
+        <div className="app w-full">
+            <header className="w-full">
+                <section className="bg-header-gradient w-[70%] h-[737px] rounded-header-radius relative pl-[116px] pr-[32px]">
+                    <div className="absolute top-[75px]">
+                        <Image
+                            width={64}
+                            height={64}
+                            src={logoSVG}
+                            alt="logo"
+                        />
+                    </div>
+                    <article className="max-w-[564px] h-full w-full flex flex-col justify-center">
+                        <h1 className="prose-heading-xl text-gunmetal mb-[2.187rem]">
+                            Body Mass Index Calculator
+                        </h1>
+                        <p className="prose-body-m text-dark-electric-blue">
+                            Better understand your weight in relation to your
+                            height using our body mass index (BM) calculator.
+                            While BMI is not the sole determinant of a healthy
+                            weight, it offers a valuable starting point to
+                            evaluate your overall health and well-being.
+                        </p>
+                    </article>
+                </section>
+            </header>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,72 @@ module.exports = {
                 border: '#D8E2E7',
                 'pure-white': '#FFFFFF',
             },
+            backgroundImage: {
+                'header-gradient':
+                    'linear-gradient(290.1deg, #D6E6FE 0%, rgba(214, 252, 254, 0) 100%)',
+            },
+            borderRadius: {
+                'header-radius': '0px 0px 35px 35px;',
+            },
+            typography: {
+                'heading-xl': {
+                    css: {
+                        fontSize: '4rem',
+                        fontWeight: 600,
+                        fontFamily: 'Inter',
+                        lineHeight: '110%',
+                    },
+                },
+                'heading-l': {
+                    css: {
+                        fontSize: '3rem',
+                        fontWeight: 600,
+                        fontFamily: 'Inter',
+                        lineHeight: '110%',
+                    },
+                },
+                'heading-m': {
+                    css: {
+                        fontSize: '1.5rem',
+                        fontWeight: 600,
+                        fontFamily: 'Inter',
+                        lineHeight: '110%',
+                    },
+                },
+                'heading-s': {
+                    css: {
+                        fontSize: '1.25rem',
+                        fontWeight: 600,
+                        fontFamily: 'Inter',
+                        lineHeight: '110%',
+                    },
+                },
+                'body-m': {
+                    css: {
+                        fontSize: '1rem',
+                        fontWeight: 400,
+                        fontFamily: 'Inter',
+                        lineHeight: '150%',
+                    },
+                },
+                'body-m-bold': {
+                    css: {
+                        fontSize: '1rem',
+                        fontWeight: 600,
+                        fontFamily: 'Inter',
+                        lineHeight: '150%',
+                    },
+                },
+                'body-s': {
+                    css: {
+                        fontSize: '0.875rem',
+                        fontWeight: 400,
+                        fontFamily: 'Inter',
+                        lineHeight: '150%',
+                    },
+                },
+            },
         },
     },
-    plugins: [],
+    plugins: [require('@tailwindcss/typography')],
 }

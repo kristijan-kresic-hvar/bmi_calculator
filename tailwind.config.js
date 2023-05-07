22043f40b474f88b9689ceeed1d7b34ac43f331e
kkresic <kristijan@agilno.com>
2023-05-07 05:39:25 +0200

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'primary-blue': '#345FF6',
+                gunmetal: '#253347',
+                'dark-electric-blue': '#5E6E85',
+                border: '#D8E2E7',
+                'pure-white': '#FFFFFF',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
